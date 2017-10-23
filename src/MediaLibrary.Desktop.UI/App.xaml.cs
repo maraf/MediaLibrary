50c8f7e6a1d637ab056de67ddfd31326e530c697
Marek Fišera <mara@neptuo.com>
2017-10-23 19:10:24 +0200

    Create empty UI project.

---
+++ src/MediaLibrary.Desktop.UI/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MediaLibrary.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MediaLibrary
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+            window.Show();
+        }
+    }
+}
