bb8500251ae5b0e2c5effb042f5199c50cefa9df
Marek Fišera <mara@neptuo.com>
2017-10-31 19:33:24 +0100

    Refactor standard save buttons to control.

---
+++ src/MediaLibrary.Desktop.UI/Views/Controls/EditButtons.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaLibrary.Views.Controls
+{
+    public partial class EditButtons : UserControl
+    {
+        public event Action SaveClick;
+        public event Action CloseClick;
+
+        public EditButtons()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e) => SaveClick?.Invoke();
+        private void btnClose_Click(object sender, RoutedEventArgs e) => CloseClick?.Invoke();
+    }
+}
