7c568f44dc6240bb4d46acc5d99d7e5f94554798
Marek Fišera <mara@neptuo.com>
2017-10-31 18:33:59 +0100

    Add simple DateTime field view.

---
+++ src/Neptuo.PresentationModels.UI.Wpf/FieldViews/WpfDateFieldView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Neptuo.PresentationModels.UI.FieldViews
+{
+    public class WpfDateFieldView : FieldView<DateTime, IWpfRenderContext>
+    {
+        private TextBox textBox;
+
+        public WpfDateFieldView(IFieldDefinition fieldDefinition)
+            : base(fieldDefinition)
+        { }
+
+        protected override void RenderInternal(IWpfRenderContext context, DateTime defaultValue)
+        {
+            textBox = new TextBox();
+            TrySetValueInternal(defaultValue);
+
+            if (FieldDefinition.Metadata.TryGet("IsReadOnly", out bool isReadOnly))
+                textBox.IsReadOnly = isReadOnly;
+
+            context.Add(textBox);
+        }
+
+        protected override bool TryGetValueInternal(out DateTime value)
+        {
+            if (textBox != null && DateTime.TryParse(textBox.Text, out value))
+                return true;
+
+            value = DateTime.MinValue;
+            return false;
+        }
+
+        protected override bool TrySetValueInternal(DateTime value)
+        {
+            if (textBox != null)
+            {
+                textBox.Text = value.ToString();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
