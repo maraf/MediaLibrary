6d9eb6e3eb550ff9bdda92eba6fa99bd1646fc97
Marek Fišera <mara@neptuo.com>
2017-11-01 20:59:01 +0100

    Add attributes for type models.

---
+++ src/Neptuo.PresentationModels.UI.Metadata/GridAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Neptuo.PresentationModels.TypeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.PresentationModels.UI
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class GridAttribute : Attribute, IMetadataReader
+    {
+        private int? column;
+        private int? row;
+
+        public int Column
+        {
+            get { return column ?? 0; }
+            set { column = value; }
+        }
+
+        public int Row
+        {
+            get { return row ?? 0; }
+            set { row = value; }
+        }
+
+        public void Apply(IMetadataBuilder builder)
+        {
+            if (column != null)
+                builder.AddGridColumn(column.Value);
+
+            if (row != null)
+                builder.AddGridRow(row.Value);
+        }
+    }
+}
---
+++ src/Neptuo.PresentationModels.UI.Metadata/GridRowSpanAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Neptuo.PresentationModels.TypeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.PresentationModels.UI
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class GridRowSpanAttribute : Attribute, IMetadataReader
+    {
+        public int Span { get; set; }
+
+        public GridRowSpanAttribute(int span)
+        {
+            Ensure.Positive(span, "span");
+            Span = span;
+        }
+
+        public void Apply(IMetadataBuilder builder)
+        {
+            builder.AddGridRowSpan(Span);
+        }
+    }
+}
