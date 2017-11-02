ea767ebc65b108687dad2ac565b29155a2dc2e4c
Marek Fišera <mara@neptuo.com>
2017-11-02 18:21:52 +0100

    Add label attribute.

---
+++ src/Neptuo.PresentationModels.UI.Metadata/LabelAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Neptuo.PresentationModels.TypeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.PresentationModels.UI
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class LabelAttribute : Attribute, IMetadataReader
+    {
+        public string Label { get; set; }
+
+        public LabelAttribute(string label)
+        {
+            Label = label;
+        }
+
+        public void Apply(IMetadataBuilder builder)
+        {
+            builder.AddLabel(Label);
+        }
+    }
+}
