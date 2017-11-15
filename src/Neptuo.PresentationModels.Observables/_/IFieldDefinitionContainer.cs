fd3f245efe5e420f24a9a5141d0576a034d52cc0
Marek Fišera <mara@neptuo.com>
2017-11-15 18:37:31 +0100

    Add container for field definition.

---
+++ src/Neptuo.PresentationModels.Observables/_/IFieldDefinitionContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.PresentationModels
+{
+    /// <summary>
+    /// A component holding instance of <see cref="IFieldDefinition"/>.
+    /// </summary>
+    public interface IFieldDefinitionContainer
+    {
+        /// <summary>
+        /// Gets an definition of field.
+        /// </summary>
+        IFieldDefinition FieldDefinition { get; }
+    }
+}
